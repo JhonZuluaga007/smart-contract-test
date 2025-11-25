78c50f88bdd09f3c83b1af8e60a6d12440b49aec
JhonZuluaga007 <jhonsalazar93@hotmail.com>
2025-11-25 12:43:34 -0500

    Initial implementation of the hardhat project is carried out.

---
+++ hardhat.config.ts
@@ -0,0 +1,39 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+
+const config: HardhatUserConfig = {
+  solidity: {
+    version: "0.8.24",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+  networks: {
+    hardhat: {
+      chainId: 5777,
+    },
+    localhost: {
+      url: "http://127.0.0.1:8545",
+      chainId: 5777,
+    },
+  },
+  paths: {
+    sources: "./contracts",
+    tests: "./test",
+    cache: "./cache",
+    artifacts: "./artifacts",
+  },
+  typechain: {
+    outDir: "typechain-types",
+    target: "ethers-v6",
+  },
+  mocha: {
+    timeout: 40000,
+  },
+};
+
+export default config;
+
